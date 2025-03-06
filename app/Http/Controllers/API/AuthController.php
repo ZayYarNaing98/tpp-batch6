52048b2ebb8ec894da0ef36e869c1f6febd47414
ZayYarNaing <zayarnaing.pp@gmail.com>
2025-03-06 15:47:56 +0630

    Add Jwt middleware

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\API\BaseController;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends BaseController
+{
+    public function login(Request $request)
+    {
+        try{
+            $credentials = $request->only(['email', 'password']);
+            // dd($credentials);
+
+            if(!JWTAuth::attempt($credentials)) {
+                return $this->error("Your email and password doesn't match");
+            }
+
+            $user = User::where('email', $credentials['email'])->first();
+
+            $payload = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'phone' => $user->phone,
+                'address' => $user->address,
+                'status' => $user->status === 1 ? 'Active' : 'Suspend'
+            ];
+
+            $token = JWTAuth::customClaims($payload)->attempt(['email' => $user->email, 'password' => $credentials['password']]);
+
+            return $this->success($token, 'User Login Successfully!', 200);
+
+        } catch(Exception $e) {
+            return $this->error($e->getMessage() ? $e->getMessage() : "Something went wrong!", null, $e->getCode() ? $e->getCode() : 500);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,17 @@ Route::get('/', function () {
 });
 
 // Category
-// Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 
-// Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-// Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
 
-// Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
+Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
 
-// Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-// Route::post('/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::post('/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
 
-// Route::post('/categories/{id}', [CategoryController::class, 'delete'])->name('categories.delete');
+Route::post('/categories/{id}', [CategoryController::class, 'delete'])->name('categories.delete');
 
 
 // Product
